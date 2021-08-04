face02deca2c581fc56cdf25a8d2c31a3b97e337
bilord <igor.buksheff@yandex.ru>
2021-08-04 12:38:43 +0300

    some fix

--- pages/_document.js
+++ pages/_document.js
@@ -20,7 +20,7 @@ export default class MyDocument extends Document {
             content="Всё, что нужно для вашего Образования."
           />
           <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-          <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico" />
+          <link rel="icon" type="image/x-icon" href="https://xieffect.vercel.app/favicon/favicon.ico" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
           <link rel="manifest" href="/favicon/site.webmanifest" />
